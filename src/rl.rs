a99cbfca7dffbb1012ef400439a40f9530d5eec9
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2014-09-29 04:17:55 +0300

    Use termutils, can now multiplex terminal+stdin reads.

--- src/rl.rs
+++ src/rl.rs
@@ -1,8 +1,12 @@
+#![feature(globs)]
+extern crate termutils;
+
 use std::{os,io};
 use std::io::BufferedReader;
+use termutils::hexes::*;
 
 fn main() {
-    let (tx, rx): (Sender<String>, Receiver<String>) = channel();
+    let (line_tx, line_rx): (Sender<String>, Receiver<String>) = channel();
 
     spawn(proc() {
         let mut stream = io::stdin();
@@ -12,14 +16,39 @@ fn main() {
                     println!("Input/output error: {}", e);
                 }
                 Ok(line) => {
-                    tx.send(line);
+                    line_tx.send(line);
                 }
             }
         }
     });
 
+    let (_, rows) = termutils::ios::size();
+    let term = Term::new();
+    let mut term_reader = term.r;
+    let mut term_writer = term.w;
+    let (line_tx, keypress_rx): (Sender<Keypress>, Receiver<Keypress>) = channel();
+    spawn(proc() {
+        loop {
+            match term_reader.read() {
+                None => break,
+                Some(k) => line_tx.send(k),
+            }
+        }
+    });
+
     loop {
-        let s = rx.recv();
-        print!("LINE: {}", s);
+        select!(
+            line = line_rx.recv() => {
+                term_writer.write("LINE: ");
+                term_writer.write(line.as_slice());
+                term_writer.flush();
+            },
+            key = keypress_rx.recv() => {
+                term_writer.write("KEY: ");
+                term_writer.write(format!("{}", key).as_slice());
+                term_writer.write("\n");
+                term_writer.flush();
+            }
+        );
     }
 }
