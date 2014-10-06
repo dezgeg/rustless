8ab5f34d227801dc957322db23852038f42157e8
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2014-10-06 04:38:22 +0300

    It's an usable pager now!

--- src/rl.rs
+++ src/rl.rs
@@ -1,54 +1,98 @@
 #![feature(globs)]
 extern crate termutils;
 
-use std::{os,io};
-use std::io::BufferedReader;
+use std::{io};
+use std::vec::{Vec};
 use termutils::hexes::*;
+use std::time::duration;
 
-fn main() {
-    let (line_tx, line_rx): (Sender<String>, Receiver<String>) = channel();
+fn doit(line_rx: Receiver<String>, keypress_rx: Receiver<Keypress>, term_writer: &mut TermWriter) {
+    let (_, term_rows) = termutils::ios::size();
 
-    spawn(proc() {
-        let mut stream = io::stdin();
-        for r in stream.lines() {
-            match r {
-                Err(e) => {
-                    println!("Input/output error: {}", e);
-                }
-                Ok(line) => {
-                    line_tx.send(line);
-                }
-            }
-        }
-    });
-
-    let (_, rows) = termutils::ios::size();
-    let term = Term::new();
-    let mut term_reader = term.r;
-    let mut term_writer = term.w;
-    let (line_tx, keypress_rx): (Sender<Keypress>, Receiver<Keypress>) = channel();
-    spawn(proc() {
-        loop {
-            match term_reader.read() {
-                None => break,
-                Some(k) => line_tx.send(k),
-            }
-        }
-    });
+    // State variables:
+    let mut lines_printed = 0u;
+    let mut topmost_line = 0u;
+    let mut lines : Vec<String> = Vec::new();
 
     loop {
         select!(
-            line = line_rx.recv() => {
-                term_writer.write("LINE: ");
-                term_writer.write(line.as_slice());
-                term_writer.flush();
+            mut line = line_rx.recv() => {
+                line.pop();
+                lines.push(line.clone());
+                lines_printed += 1;
+
+                if lines_printed <= term_rows {
+                    term_writer.write(line.as_slice());
+                    if lines_printed < term_rows {
+                        term_writer.write("\n");
+                    }
+                    term_writer.flush();
+                }
+
             },
             key = keypress_rx.recv() => {
-                term_writer.write("KEY: ");
-                term_writer.write(format!("{}", key).as_slice());
-                term_writer.write("\n");
-                term_writer.flush();
+                if lines_printed < term_rows  {
+                    continue
+                }
+                match key {
+                    KeyUp => {
+                        if topmost_line > 0 {
+                            topmost_line -= 1;
+                            term_writer.move_cursor(0, 0);
+                            term_writer.scroll_reverse();
+                            term_writer.write(lines[topmost_line].as_slice());
+                            term_writer.flush();
+                        }
+                    }
+                    KeyDown => {
+                        if topmost_line + term_rows < lines.len() {
+                            topmost_line += 1;
+                            term_writer.move_cursor(0, term_rows - 1);
+                            term_writer.scroll_forward();
+                            term_writer.write(lines[topmost_line + term_rows - 1].as_slice());
+                            term_writer.flush();
+                        }
+                    }
+                    _ => (),
+                }
             }
         );
     }
+}
+
+fn main() {
+    termutils::ios::preserve(|| {
+        let (line_tx, line_rx): (Sender<String>, Receiver<String>) = channel();
+
+        spawn(proc() {
+            let mut stream = io::stdin();
+            for r in stream.lines() {
+                match r {
+                    Err(e) => {
+                        println!("Input/output error: {}", e);
+                    }
+                    Ok(line) => {
+                        line_tx.send(line);
+                    }
+                }
+            }
+            io::timer::sleep(duration::MAX); // XXX FIXME, just block the task
+        });
+
+        let term = Term::new();
+        let mut term_reader = term.r;
+        let mut term_writer = term.w;
+        let (line_tx, keypress_rx): (Sender<Keypress>, Receiver<Keypress>) = channel();
+        spawn(proc() {
+            loop {
+                match term_reader.read() {
+                    None => break,
+                    Some(k) => line_tx.send(k),
+                }
+            }
+        });
+
+        // term_writer.cursor(true); // TODO
+        doit(line_rx, keypress_rx, &mut term_writer);
+    });
 }
